14f46ca3323c0fceb9419bd56b4bb0e80258b673
hex <ms0723595@gmail.com>
2017-08-07 22:30:28 +0800

    final upload

--- scripts/main.js
+++ scripts/main.js
@@ -1,14 +1,12 @@
-
-
-window.onload = function() {
+window.onload=function(){
     init();
 };
 
-var mode = 'hard'; // 'easy', 'hard', or 'nightmare'
-var numCards = 6;
-var gameOver = false;
+var mode = 'hard' ;
+var numCards= 6 ;
+var gameover = false ;
 var colors = [];
-var pickedColor;
+var pickedColor ;
 var body = document.querySelector("body");
 var cards = document.querySelectorAll(".card");
 var colorDisplay = document.getElementById("color-picked");
@@ -21,148 +19,134 @@ var countdownDisplay = document.querySelector("#countdown");
 var countdownId;
 var blinkId;
 
-function init() {
-    initModeButtons();
+function init(){
+    initButtons();
     initCards();
     reset();
 }
 
-function initModeButtons() {
-    for (var i = 0; i < modeButtons.length; i++) {
-        modeButtons[i].addEventListener("click", function() {
-            for (var j = 0; j < modeButtons.length; j++) {
-                modeButtons[j].classList.remove("selected");
+function initButtons(){
+    for(var i =0;i < modeButtons.length; i++){
+        modeButtons[i].addEventListener("click", function(){
+            for(var j=0 ; j<modeButtons.length; j++){
+                modeButtons[j].classList.remove("selected") ;   
             }
             this.classList.add("selected");
             mode = this.textContent.toLowerCase();
-            switch (mode) {
-                case 'easy':
+            switch (mode){
+                case 'easy' :
                     numCards = 3;
-                    break;
-                default: // 'hard' or 'nighmare'
-                    numCards = 6;
+                    break ;
+                default :
+                    numCards = 6 ;  
             }
             reset();
         });
     }
 }
 
-function initCards() {
-    for (var i = 0; i < cards.length; i++) {
-        //add click listeners to cards
-        cards[i].addEventListener("click", function() {
-            if (gameOver)
+function initCards(){
+    for(var i = 0; i < cards.length; i++){
+        cards[i].addEventListener("click",function(){
+            if (gameover)
                 return;
-            //grab color of clicked card
             var clickedColor = this.style.backgroundColor;
-            // alert(this.style.backgroundColor);
-            //compare color to pickedColor
-            if (clickedColor === pickedColor) {
+
+            if(clickedColor === pickedColor){
                 clearInterval(countdownId);
                 messageDisplay.textContent = "Correct!";
                 end();
             } else {
-                this.style.opacity = 0;
-                messageDisplay.textContent = "Try Again"
+                this.style.opacity = 0 ;
+                messageDisplay.textContent = "Try Again" ;
             }
-        });
+        })
     }
 }
 
-function end() {
+function end(){
     countdownDisplay.textContent = '';
-    resetButton.style.opacity = 1;
-    resetDisplay.textContent = "Play Again"
+    resetButton.style.opacity = 1 ;
+    resetDisplay.textContent = "Play Again";
     changeColors("#FFF");
     body.style.backgroundColor = pickedColor;
-    gameOver = true;
+    gameover = true ;
 }
 
-function reset() {
+function reset(){
     clearInterval(countdownId);
-    gameOver = false;
-    if (mode === 'nightmare') {
-        resetButton.style.opacity = 0;
-        countdownDisplay.innerHTML = '&nbsp;&nbsp;' + '5';
-        countdownId = setInterval(countdown, 1000);
-    } else {
-        countdownDisplay.textContent = '';
+    gameover = false ;
+    if(mode === 'nightmare'){
+        resetButton.style.opacity = 0 ;
+        countdownDisplay.innerHTML = '&nbsp;&nbsp;'+'3';
+        countdownId = setInterval(countdown,1000);
+    }else{
+        countdownDisplay.textContent = ' '; 
     }
     colors = generateRandomColors(numCards);
-    //pick a new random color from array
     pickedColor = pickColor();
-    //change colorDisplay to match picked Color
     colorDisplay.textContent = pickedColor;
-    resetDisplay.textContent = "New Color"
+    resetDisplay.textContent = "New Color";
     messageDisplay.textContent = "What's the Color?";
-    //change colors of cards
-    for (var i = 0; i < cards.length; i++) {
-        cards[i].style.opacity = 1;
-        if (colors[i]) {
-            cards[i].style.display = "block";
+
+    for(var i = 0 ; i < cards.length;i++){
+        cards[i].style.opacity = 1 ;
+        if(colors[i]){
+            cards[i].style.display = "block" ;
             cards[i].style.backgroundColor = colors[i];
         } else {
             cards[i].style.display = "none";
         }
     }
-    body.style.backgroundColor = "#232323";
+    body.style.backgroundColor = "#232323" ;    
 }
 
-resetButton.addEventListener("click", function() {
+
+resetButton.addEventListener("click",function(){
     reset();
 })
 
-function changeColors(color) {
-    //loop through all cards
-    for (var i = 0; i < cards.length; i++) {
-        //change each color to match given color
-        cards[i].style.opacity = 1;
-        cards[i].style.backgroundColor = color;
+function changeColors(color){
+    for(var i = 0 ; i < cards.length; i++){
+        cards[i].style.opacity = 1 ;
+        cards[i].style.backgroundColor = color ;    
     }
 }
 
-function pickColor() {
-    var random = Math.floor(Math.random() * colors.length);
-    return colors[random];
+function pickColor(){
+    var random = Math.floor(Math.random() *  colors.length);
+    return colors[random] ;
 }
 
-function generateRandomColors(num) {
-    //make an array
-    var arr = []
-    //repeat num times
-    for (var i = 0; i < num; i++) {
-        //get random color and push into arr
+function generateRandomColors(num){
+    var arr = [] ;
+    for (var i=0 ; i < num ;i++)
+    {
         arr.push(randomColor())
     }
-    //return that array
-    return arr;
+    return arr ;
 }
 
-function randomColor() {
-    //pick a "red" from 0 - 255
-    var r = Math.floor(Math.random() * 256);
-    //pick a "green" from  0 -255
-    var g = Math.floor(Math.random() * 256);
-    //pick a "blue" from  0 -255
-    var b = Math.floor(Math.random() * 256);
+function randomColor(){
+    var r = Math.floor(Math.random()*256);
+    var g = Math.floor(Math.random()*256);
+    var b = Math.floor(Math.random()*256);
     return "rgb(" + r + ", " + g + ", " + b + ")";
 }
 
-function countdown() {
+function countdown(){
     var c = Number(countdownDisplay.textContent);
     countdownDisplay.innerHTML = '&nbsp;&nbsp;' + (--c);
-    if (c === 0) {
+    if(c === 0){
         clearInterval(countdownId);
-        messageDisplay.textContent = "Timeout!";
+        messageDisplay.textContent = "TimeOut!" ;
         end();
-    } else {
-        body.style.backgroundColor = '#FFF';
-        blinkId = setInterval(blink, 50);
+    }else{
+        body.style.backgroundColor = '#FFF' ;
+        blinkId = setInterval(blink , 50);
     }
 }
 
-function blink() {
-    body.style.backgroundColor = '#232323';
-    clearInterval(blinkId);
+function blink(){
+    body.style.backgroundColor = '#232323' ;
 }
-
